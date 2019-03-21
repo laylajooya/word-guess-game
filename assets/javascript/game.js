22152d84f7b4b5b2e1da6488f7ae6d89c2278947
Layla Jooya <laylajooya@gmail.com>
2019-03-21 09:37:24 -0500

    game refreshes when u win yayy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,13 +9,16 @@ var underscoreText = document.getElementById("underscore-text");
 var guessesLeftText = document.getElementById("guessesLeft-text");
 var alreadyGuessedText = document.getElementById("alreadyGuessed-text");
 var winsText = document.getElementById ("wins-text");
+var gameAlertText = document.getElementById ("gameAlert-text");
 
+// ----------------- GAME START ---------- //
 // choose word randomly
 var random = Math.floor(Math.random() * word.length);
 var wordPick = word[random];
 var wordPickLetter = wordPick.split("");
 
 console.log(wordPick);
+
 
 // make underscores based on word length
 var generateUnderscore = function () {
@@ -32,22 +35,65 @@ underscoreText.textContent = "" + underscore.join(" ");
 guessesLeftText.textContent = "Number of guesses remaining: " + guessesLeft;
 alreadyGuessedText.textContent = "Letters already guessed: " + alreadyGuessed;
 winsText.textContent = "Wins: " + wins;
+
+// ----------------- PLAYING GAME ---------- //
+function resetGame (){
+    guessesLeft = 10;
+    alreadyGuessed = [];
+    wordUsedInLastGame = wordPick;
+
+    // at start of game lets say var random got set to 0, so wordPick would then be "505"
+
+    // so here random is still 0, and splice allows you to remove an item in an array at a certain index, in this case would be 0
+    // so word[0] = "505"  ....   word.splice(0, 1)  the 1 just means we want to remove 1 item
+    // so now the array is ["sheffield", "turner"]
+    // but we have the old word "505" saved in wordUsedInLastGame
+
+    //so once 505 is out and they play sheffield then sheffield is 0 and turner will be 1??
+    // exactly --- but it won't really matter cuz now `random` below will only be between 0 or 1 and picking between those two options
+    // then once we've picked the new word below
+    // see how we do the word.push("505") back into it
+    // so then the array will be back to ["sheffield", "turner", "505"]
+
+    // and when you rerun this function let's say sheffield was the next word that got picked, the array for the next word selection would be 
+    // ["turner", "505"]
+
+    word.splice(random, 1);
+    
+
+
+    // array at this point should be [" ", " "] instead of [" ", " ", " "]
+    // so the same word will never get used in back to back rounds
+
+    random = Math.floor(Math.random() * word.length);
+    wordPick = word[random];
+    wordPickLetter = wordPick.split("");
+
+    // pushing back word into array to be used for next time
+    word.push(wordUsedInLastGame)
+    console.log(wordPick);
+    
+    generateUnderscore ();
+
+}
 // user guess
 document.onkeyup = function(event) {
     var userGuess = event.key;
     alreadyGuessed.push(userGuess);
 
-    for (var i=0; i < wordPickLetter.length; i++){
+    for (var i = 0; i < wordPickLetter.length; i++){
         if (wordPickLetter[i] == userGuess.toString()) {
             underscore[i] = userGuess;
         }
     }
 
     if (wordPick == underscore.join("")){
-        wins++; guessesLeft = 10; alreadyGuessed = [];
+        gameAlertText.textContent = "You win!";
+        wins++; resetGame ();
     } else {
         if (guessesLeft == 0) {
-        alreadyGuessed = [];
+        gameAlertText.textContent = "You lost!";
+        resetGame ();
         } else {
             guessesLeft--;
         }
@@ -62,20 +108,3 @@ document.onkeyup = function(event) {
 
 
 
-
-    // checking if letter matches in word
-    // if(wordPick.indexOf(userGuess) > -1){
-        
-    //     alreadyGuessed.push(userGuess);
-    //     console.log("alreadyGuessed" + alreadyGuessed);
-    //     // push letter to right underscore
-    //     underscore [wordPick.indexOf(userGuess)] = userGuess;
-    //     console.log("underscore:" + underscore);
-    //     // if word matches
-    //     if (underscore.join("") === wordPick){
-    //         wins++; 
-    //         guessesLeft = 10;
-    //     }
-
-    //     //  else i lose a guess
-    // }
